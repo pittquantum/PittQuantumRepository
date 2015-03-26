ee11b11c5299aa2579060ec941d3237fc882f8ed
Josh Rogan <jjjr1122@gmail.com>
2015-03-25 20:41:49 -0400

    Lots

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "PQR: coming soon"
+	return render_template("home.html")
+	# return "Hello"
 
 @app.route('/mol/<key>')
 @app.route('/mol/<key>/')
 def molecule(key):
 	return "Test: " + key
+
+@app.route('/news')
+def news():
+	return "PQR: News"
+
+@app.route('/browse')
+def browse():
+	return "PQR: Browse"
+
+
 
 if __name__ == '__main__':
 	app.run()
