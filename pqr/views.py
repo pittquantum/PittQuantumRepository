2d13f03c1a6a85aae426e6f97b79c160a2f5788a
Josh Rogan <jjjr1122@gmail.com>
2015-03-27 18:52:25 -0400

    Added Macros, better templating, and organization

---
+++ pqr/views.py
@@ -0,0 +1,47 @@
+#!./venv/bin/python
+from flask import render_template
+from pqr import pqr
+
+MOLECULE_OF_THE_DAY = 999
+
+@pqr.route('/')
+@pqr.route('/home')
+@pqr.route('/home/')
+def index():
+	page = {'id': "page-home"}
+	return render_template("home.html", page = page)
+
+@pqr.route('/mol/<key>')
+@pqr.route('/mol/<key>/')
+@pqr.route('/mol/') #if no key lets default to the molecule of the day
+@pqr.route('/mol') #if no key lets default to the molecule of the day
+def molecule(key = -1):
+	if key == -1:
+		key = MOLECULE_OF_THE_DAY #Molecule of the day value
+	page = {'id': "page-molecule", 'moleculeKey': key}
+	return render_template("molecule.html", page = page)
+
+@pqr.route('/news')
+@pqr.route('/news/')
+def news():
+	page = {'id': "page-news"}
+	return render_template("news.html", page = page)
+
+@pqr.route('/browse')
+@pqr.route('/browse/')
+def browse():
+	page = {'id': "page-browse"}
+	return render_template("browse.html", page = page)
+
+@pqr.route('/contact')
+@pqr.route('/contact/')
+def contact():
+	page = {'id': "page-contact"}
+	return render_template("contact.html", page = page)
+
+@pqr.errorhandler(404)
+def page_not_found(e):
+	return "404"
+
+if __name__ == '__main__':
+	pqr.run()
---
+++ server.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from pqr import pqr
+
+pqr.run(debug = True)
