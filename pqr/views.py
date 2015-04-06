fb0b72d4b3d78f2adc4de9e4bd58acc14301c797
Ritwik Gupta <ritwikg2004@live.com>
2015-04-06 01:47:06 -0400

    Made views follow PEP8 conventions and modified gitignore

--- pqr/views.py
+++ pqr/views.py
@@ -1,12 +1,12 @@
 #!./venv/bin/python
 from flask import render_template, url_for, redirect, flash, send_from_directory
-from flask.ext.cache import Cache
+# from flask.ext.cache import Cache
 from pqr import pqr
 from settings import APP_JSON
 import os
 import json
 
-#cache = Cache(pqr,config={'CACHE_TYPE': 'simple'})
+# cache = Cache(pqr,config={'CACHE_TYPE': 'simple'})
 MOLECULE_OF_THE_WEEK = 'GZCGUPFRVQAUEE-SLPGGIOYSA-N'
 
 ###############################################################################################################
@@ -14,70 +14,76 @@ MOLECULE_OF_THE_WEEK = 'GZCGUPFRVQAUEE-SLPGGIOYSA-N'
 @pqr.route('/home')
 @pqr.route('/home/')
 def index():
-	page = {'id': "page-home"}
-	return render_template("home.html", page = page)
+    page = {'id': "page-home"}
+    return render_template("home.html", page=page)
+
 
 ###############################################################################################################
 @pqr.route('/mol/<key>')
 @pqr.route('/mol/<key>/')
-@pqr.route('/mol/') #if no key lets default to the molecule of the day
-@pqr.route('/mol') #if no key lets default to the molecule of the day
-#@cache.cached(timeout=5)
-def molecule(key = -1):
-	if key == -1:
-		key = MOLECULE_OF_THE_WEEK
+@pqr.route('/mol/')  # if no key lets default to the molecule of the day
+@pqr.route('/mol')  #if no key lets default to the molecule of the day
+# @cache.cached(timeout=5)
+def molecule(key="-1"):
+    if key == "-1":
+        key = MOLECULE_OF_THE_WEEK
 
-	keyFirstTwo = key[:2]
+    key_first_two = key[:2]
 
-	page = {'id': "page-molecule", 'moleculeKey': key, 'keyFirstTwo': keyFirstTwo}
+    page = {'id': "page-molecule", 'moleculeKey': key, 'key_first_two': key_first_two}
 
-	try:
-		with open(os.path.join(APP_JSON, keyFirstTwo + '/' + key + '.json')) as j:
-			jsonDict = json.load(j)
-	except IOError:
-		flash("You entered a molecule that didn't exist, so you've been redirected to the molecule of the week!")
-		return redirect(url_for('molecule', key=MOLECULE_OF_THE_WEEK))
+    try:
+        with open(os.path.join(APP_JSON, key_first_two + '/' + key + '.json')) as j:
+            json_dict = json.load(j)
+    except IOError:
+        flash("You entered a molecule that didn't exist, so you've been redirected to the molecule of the week!")
+        return redirect(url_for('molecule', key=MOLECULE_OF_THE_WEEK))
 
-	# return the view
-	return render_template("molecule.html", page = page, jsonDict = jsonDict)
+    # return the view
+    return render_template("molecule.html", page=page, jsonDict=json_dict)
+
 
 ###############################################################################################################
 @pqr.route('/news')
 @pqr.route('/news/')
 def news():
-	page = {'id': "page-news"}
+    page = {'id': "page-news"}
 
-	return render_template("news.html", page = page)
+    return render_template("news.html", page=page)
+
 
 ###############################################################################################################
 @pqr.route('/browse')
 @pqr.route('/browse/')
 def browse():
-	page = {'id': "page-browse"}
+    page = {'id': "page-browse"}
 
-	return render_template("browse.html", page = page)
+    return render_template("browse.html", page=page)
+
 
 ###############################################################################################################
 @pqr.route('/contact')
 @pqr.route('/contact/')
 def contact():
-	page = {'id': "page-contact"}
+    page = {'id': "page-contact"}
 
-	return render_template("contact.html", page = page)
+    return render_template("contact.html", page=page)
+
 
 ###############################################################################################################
-#Properly handle the favicon 
+# Properly handle the favicon
 @pqr.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(pqr.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 ###############################################################################################################
 
 ###############################################################################################################
 @pqr.errorhandler(404)
 def page_not_found(e):
-	molecule(key=MOLECULE_OF_THE_WEEK)
+    molecule(MOLECULE_OF_THE_WEEK)
 
 ###############################################################################################################
 
@@ -87,4 +93,4 @@ pqr.secret_key = ';t}UzRZmis-xueR*5Hh:F={7?2^|.mPxW-`@*||L]]y]:h7v[A4TCn_:[j{-:+
 ###
 
 if __name__ == '__main__':
-	pqr.run()
+    pqr.run()
