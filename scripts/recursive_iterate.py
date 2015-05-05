432f600dde6f7269a6edb4e24966221b71c0610d
JoshuaRogan <jjjr1122@gmail.com>
2015-05-05 18:31:12 -0400

    Building database

--- scripts/recursive_iterate.py
+++ scripts/recursive_iterate.py
@@ -2,10 +2,78 @@
 # Otherwise, and we should do this, the path in os.walk()
 # should be replaced with the relative path from the python
 # file that will run this file
-
 import os
+import json
+from pymongo import MongoClient
+from datetime import datetime
 from os.path import join, getsize
+from pprint import pprint
 
-for root, dirs, files in os.walk('../pqr/static/data/json'): # This path to replace
+DIRECTORY = '../pqr/static/data/json/'
+
+client = MongoClient()
+db = client.test
+
+for root, dirs, files in os.walk(DIRECTORY): # This path to replace
     for file in files:
-        print file #replace this print with insert to Mongo
+    	# print file
+        json_file = open(DIRECTORY + file[:2] + "/" + file, "r")
+        json_data = json.load(json_file)
+
+
+
+
+
+        ##Creating a properties document
+        properties_id = db.properties.insert_one({
+        	"molecular mass": json_data['molecular mass'], 
+        	"inchi": json_data['inchi']
+        }).inserted_id #Use this to link the molcules 
+
+
+
+
+
+
+
+        ##Mol2 File (Ignore for now)
+        
+
+
+
+
+        ##Creating a molcules document use the properties id to link
+        	##For now lets just have a link here to the mol2 data 
+        molecules_id = db.molecules.insert_one({
+        	"last_updated": datetime.now(), 
+        	"properties_id": properties_id, 
+        	"inchikey": json_data['inchi'], 
+        	"name": json_data['name'], 
+        	"formula": json_data['formula']
+        }).inserted_id #
+
+
+
+
+
+
+
+
+
+        # pprint(data)
+        json_file.close()
+
+#########################
+
+
+##Show all properties being created  
+cursor = db.properties.find()
+for document in cursor: 
+	print(document)
+
+##Show all molecules being created 
+cursor = db.molecules.find()
+for document in cursor: 
+	print(document)
+
+        
