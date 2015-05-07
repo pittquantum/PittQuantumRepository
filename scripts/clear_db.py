9b5c31cde1a6a11657e4a350db7b9e407841044b
Ritwik Gupta <ritwikg2004@live.com>
2015-05-06 23:42:53 -0400

    updated clear db

--- scripts/clear_db.py
+++ scripts/clear_db.py
@@ -4,12 +4,5 @@ from pprint import pprint
 client = MongoClient()
 db = client.test
 
-print "# Properties Documents: " + str(db.properties.count())
-print "# Molecule Documents: " + str(db.molecules.count())
-
-print "Deleting"
-db.properties.drop()
-db.molecules.drop()
-
-print "# Properties Documents: " + str(db.properties.count())
-print "# Molecule Documents: " + str(db.molecules.count())
+db.properties.drop_indexes()
+db.molecules.drop_indexes()
