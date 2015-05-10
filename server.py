5b282b967525ae792dd4d4cce555b0dae5180a20
Ritwik Gupta <ritwikg2004@live.com>
2015-05-09 23:49:22 -0400

    Implemented molecule of the week update

--- server.py
+++ server.py
@@ -1,17 +1,25 @@
 #!./venv/bin/python
 from pymongo import MongoClient
+import datetime
 
 from pqr import pqr
 import pqr.views as pqv
 
 # Opens the redirect file and stores in the redirect_table
 # dictionary in the views.py file
-with open("./pqr/redirects/redirect_file", "r") as redir:
+with open("./pqr/server_start/redirect_file", "r") as redir:
     for line in redir:
         lineArr = line.strip().split(',')
         key = lineArr[0]
-        value = lineArr[1]
+        value = lineArr[1].strip()
         pqv.redirect_table[key] = value
+
+# Set the Molecule of the Week
+with open("./pqr/server_start/mol_of_the_week", "r") as molfile:
+    for line in molfile:
+        if line.strip.split(",")[0] == datetime.date.isoformat(datetime.datetime.now()):
+            pqv.MOLECULE_OF_THE_WEEK = line.strip.split(",")[1].strip()
+            break
 
 # Get number of molecules in database
 client = MongoClient()
