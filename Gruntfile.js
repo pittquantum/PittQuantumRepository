1e319dc3638de71c4630d126e773dcaa2adf022b
JoshuaRogan <jjjr1122@gmail.com>
2015-05-16 20:59:06 -0400

    Combined and minified all JS. Moved all js out of public directory to assets/js. Only pqr.js and pqr.min.js are now publically accessable

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,31 @@ module.exports = function(grunt) {
         }
       }
     },
+    concat: {
+  		options: {
+  			// define a string to put between each file in the concatenated output
+  			separator: ';'
+  		},
+  		dist: {
+  			// the files to concatenate (modernizr, then jquery, then bootstrap, then everythign else)
+  			src: ['assets/js/modernizr/**/*.js', 'assets/js/jquery/**/*.js', 'assets/js/bootstrap/**/*.js', 'assets/js/**/*.js'],
+
+  			// the location of the resulting JS file
+  			dest: 'pqr/static/js/pqr.js'
+  		}
+	},    
+	uglify: {
+		options: {
+			// define a string to put between each file in the concatenated output
+			banner: '/*! PQR JavaScript Combined and minified on <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+		},
+		dist: {
+			// the files to concatenate
+			files:{
+				"pqr/static/js/pqr.min.js": ['pqr/static/js/pqr.js']
+			} 
+		}
+	},
     watch: {
       styles: {
         files: ['pqr/static/css/**/*.less'], // which files to watch
@@ -19,11 +44,17 @@ module.exports = function(grunt) {
         options: {
           nospawn: true
         }
+      },
+      scripts:{
+      	files: ['pqr/static/js/**/*.js'],
+      	tasks: ['concat', 'uglify']
       }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('default', ['watch']);
 };
