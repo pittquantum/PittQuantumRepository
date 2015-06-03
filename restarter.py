243669118f216a6b7f58d7c05903a859ff7291a9
Ritwik Gupta <ritwikg2004@live.com>
2015-06-02 22:09:34 -0700

    Restarter asks for port now

--- restarter.py
+++ restarter.py
@@ -3,9 +3,14 @@
 import os
 import signal
 from subprocess import check_output, call
+import sys
 
-PORT = "5000"
-AFTER = ["python", "server.py", PORT] 
+if len(sys.argv) < 2:
+	print "Please enter a port number! Exiting..."
+	sys.exit()
+
+PORT = sys.argv[1]
+AFTER = ["python", "server.py", PORT]
 
 # Command: sudo netstat -ap | grep :5000
 results = check_output(["sudo", "netstat", "-ap", "|", "grep :" + PORT])
@@ -17,7 +22,7 @@ for row in dictionary:
 	values = row.split()
 	if len(values) > 6:
 		checking = values[len(values) - 1]
-		if "/" in checking: 
+		if "/" in checking:
 			program = checking.split('/')
 			if program[1] == 'python':
 				pid = program[0]
