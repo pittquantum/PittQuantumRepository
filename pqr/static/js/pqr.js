0f513e84b40feb1c3da462aa3ead7c0a368c9f4b
Josh Rogan <jjjr1122@gmail.com>
2015-09-10 03:27:20 -0400

    Add font to base tempalte

--- pqr/static/js/pqr.js
+++ pqr/static/js/pqr.js
@@ -21369,7 +21369,7 @@ pqr.init = function() {
 		pqr.bindevents.bindFontSwitchers();
 		htmlutilities.footerToBottom('footer', '#main');
 		loadCSS("//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css");
-		loadCSS("//fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,700");
+		// loadCSS("//fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,700");
 
 		if ($("#main").hasClass("page-home")) {
 			pqr.threeDMole.initViewers();
